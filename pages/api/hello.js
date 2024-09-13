5c40f644082593425abf41afac726fc5bd30a838
Ivan Cherepukhin <hi@damn.vc>
2024-09-13 01:53:08 -0400

    fucking fuck

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,3 @@
-// File: pages/api/hello.ts
-
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type ResponseData = {
-  message: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
+export default function handler(req, res) {
   res.status(200).json({ message: 'Hello from Next.js!' })
 }

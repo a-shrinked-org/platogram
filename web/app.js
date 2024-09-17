c9481894f0157a03c4a67bc2f1c211a2941bb112
Ivan Cherepukhin <hi@damn.vc>
2024-09-17 12:06:31 -0400

    styles check

--- web/app.js
+++ web/app.js
@@ -496,7 +496,7 @@ function updateUIStatus(status, message = "") {
                 doneSection.innerHTML = `
                     <p>File/URL: ${fileName}</p>
                     <p>Email: ${userEmail}</p>
-                    <p>Status: Completed</p>
+                    <p>Status: Completed</p><br>
                     ${message ? `<p>${message}</p>` : ''}
                     <button class="mx-auto block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onclick="reset()">Reset</button>
                 `;

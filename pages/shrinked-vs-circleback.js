57c46c3c088cadfb1b1987283f5fcfbd5f5db3b3
Ivan Cherepukhin <hi@damn.vc>
2024-09-19 22:48:50 -0400

    markdoc two

--- pages/shrinked-vs-circleback.js
+++ pages/shrinked-vs-circleback.js
@@ -1,40 +1,30 @@
 import Markdoc from '@markdoc/markdoc';
 import Head from 'next/head';
+import fs from 'fs';
+import path from 'path';
 
 export default function ShrinkdVsCirclebackPage({ content, frontmatter }) {
   return (
     <>
       <Head>
-        <title>{frontmatter.title}</title>
-        <meta name="description" content={frontmatter.description} />
+        <title>{frontmatter.title || 'Shrinked vs CircleBack'}</title>
+        <meta name="description" content={frontmatter.description || 'Comparison between Shrinked and CircleBack'} />
       </Head>
-      <div className="container mx-auto px-4 py-8">
+      <article className="container mx-auto px-4 py-8">
         <div dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
+      </article>
     </>
   );
 }
 
-export default function ShrinkdVsCirclebackPage({ content }) {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  );
-}
-
 export async function getStaticProps() {
-  const fs = require('fs');
-  const path = require('path');
-
-  const filePath = path.join(process.cwd(), 'shrinked-vs-circleback.md');
+  const filePath = path.join(process.cwd(), 'content', 'shrinked-vs-circleback.md');
   const markdownContent = fs.readFileSync(filePath, 'utf8');
 
   const ast = Markdoc.parse(markdownContent);
+  const frontmatter = Markdoc.frontmatter(ast);
   const content = Markdoc.transform(ast);
   const html = Markdoc.renderers.html(content);
-
-  const frontmatter = Markdoc.frontmatter(ast);
 
   return {
     props: {
